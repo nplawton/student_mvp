b469103372e964c935e61394fbad5c6d91a4e5e0
nplawton <nplawton02@gmail.com>
2023-03-07 08:07:25 -0800

    CRUD post test13

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ app.post('/creature', (req, res, next) => {
     //console.log(`${name} will attack ${attack} and ${special} and looks ${description}`);
     //console.log(`${name} image located at ${mon_img}`);
 
-    if(name && !Number.isNaN(armor) && !Number.isNaN(health) && !Number.isNaN(stre) && !Number.isNaN(dex) && !Number.isNaN(cons) && !Number.isNaN(intel) && !Number.isNaN(wis) && !Number.isNaN(charisma) && !Number.isNaN(chal)){
-        pool.query(`INSERT INTO creature (name, armor, health, stre, dex, cons, intel, wis, charisma, chal) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`, [name, armor, health, stre, dex, cons, intel, wis, charisma, chal], (err, data) => {
+    if(name && !Number.isNaN(armor) && !Number.isNaN(health) && !Number.isNaN(stre) && !Number.isNaN(dex) && !Number.isNaN(cons) && !Number.isNaN(intel) && !Number.isNaN(wis) && !Number.isNaN(charisma) && !Number.isNaN(chal) && attack && special && description && mon_img){
+        pool.query(`INSERT INTO creature (name, armor, health, stre, dex, cons, intel, wis, charisma, chal, attack, special, description, mon_img) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`, [name, armor, health, stre, dex, cons, intel, wis, charisma, chal, attack, special, description, mon_img], (err, data) => {
             const newCreature = data.rows[0];
             console.log("Creature created", newCreature);
 
