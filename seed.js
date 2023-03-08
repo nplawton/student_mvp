bd74cc72e091723c3dbbcbc32d2edee2e037cbf8
nplawton <nplawton02@gmail.com>
2023-03-08 10:02:12 -0800

    second table added2

--- seed.js
+++ seed.js
@@ -1,7 +1,35 @@
 const { Pool } = require('pg');
 const pool = require('./dbConn');
 
+pool.query(`INSERT INTO type (name, description) VALUES
+        ON CONFLICT DO NOTHING 
+        ('Aberration', 'Aberrations are alien entities, often with powers drawn from their minds. All aberrations have darkvision.'),
+        ('Beast', 'Beasts are nonhumanoid creatures that are part of the natural world. Some beasts have magical powers, but are generally low in intelligence.'),
+        ('Celestial', 'Celestials are creatures native to the Upper Planes, and good by nature.'), 
+        ('Construct', 'A construct is either an animated object of some sort, or an artificially crafted creature. They are usually unintelligent, and immune to mind-influencing effects. They are immune to many effects that harm living creatures, such as poison and disease, but they cannot be magically restored to life. Constructs usually have darkvision.'),
+        ('Dragon', 'A dragon is a reptilian creature, usually winged, often having supernatural abilities. Dragons cannot be paralyzed or induced to sleep. Dragons usually have darkvision and low-light vision.'),
+        ('Elemental', 'An elemental is composed of one of the four classical elements of air, earth, fire, or water. They can resist effects such as poison and paralysis, and do not have any discernable front or back, making tactics such as flanking or attacks such as critical hits useless. Like constructs, they cannot be resurrected by magic once destroyed, save powerful spells such as wish or miracle. Elementals usually have darkvision.'),
+        ('Fey', 'Fey are creatures of magic with a connection to nature. Often they are connected to the Feywild, or certain Outer Planes such as Arborea or the Beastlands.'),
+        ('Fiend', 'Fiends are creatures native to the Lower Planes, and evil by nature.'),
+        ('Giant', 'A giant is a humanoid-shaped creature of great strength and size. Giants usually have darkvision.'),
+        ('Humanoid', 'Humanoids are the main people of most worlds, bipeds with culture but few natural magical abilities.'),
+        ('Monstrosity', 'Monstrosities are unnatural creatures from a variety of origins, including curses and magical experimentation.'),
+        ('Ooze',	'An ooze is an amorphous or mutable creature. Oozes are usually mindless, and thus immune to mind-influencing effects. They possess blindsight but are otherwise blind. Like elementals, they have no clear front or back and are thus immune to flanking or critical hits. Composed mainly of protoplasm, oozes resist effects such as poison and polymorphing.'),
+        ('Plant', 'A plant is a vegetable and fungus creature, and immune to effects such as poison, stunning, or polymorphing. They also ignore mind-influencing effects, and may have low-light vision (assuming they can see at all).'),
+        ('Undead', 'An undead is a once-living creature animated by spiritual or supernatural forces. They resist many effects which harm the living, such as poison and disease. They are healed by negative energy. Undead usually have darkvision.'),
+        ('Vermin', 'A vermin is an invertebrate, such as an insect, arachnid, arthropod, or worm. Vermin are unintelligent and immune to mind-influencing effects. They usually have darkvision.'),
+        ('Monstrous Humanoid', 'A monstrous humanoid is similar to a humanoid, but usually has monstrous or animalistic features. Many also have supernatural abilities. Monstrous humanoids usually have darkvision.'),
+        ('Goblinoid', 'Goblinoids had a typical humanoid anatomy, though their skin tone and texture was often somewhere between that of a human and a snail. The notable exception of this rule was the bugbears whose entire bodies were covered head to toe in thick fur.')`, 
+    (err, type) => {
+        if(err){
+            console.log("Insert failed");
+        }else{
+            console.log(type);
+        }
+});
+
 pool.query(`INSERT INTO creature (name, alignment_id, type_id, health, exp, chal, descript_id, stat_id, attack_id, spattack_id) VALUES
+        ON CONFLICT DO NOTHING
         ('Aarakocra', 1, 16, 13, 50, 1/4, 1, 1, 1, 1),
         ('Aboleth', 2, 1, 135, 5900, 10, 2, 2, 2, 2),
         ('Animated Object, Armor', 4, 4, 33, 200, 1, 3, 3, 3, 3),
@@ -37,33 +65,6 @@ pool.query(`INSERT INTO creature (name, alignment_id, type_id, health, exp, chal
                 console.log(creature);
             }
 });
-
-pool.query(`INSERT INTO type (name, description) VALUES 
-        ('Aberration', 'Aberrations are alien entities, often with powers drawn from their minds. All aberrations have darkvision.'),
-        ('Beast', 'Beasts are nonhumanoid creatures that are part of the natural world. Some beasts have magical powers, but are generally low in intelligence.'),
-        ('Celestial', 'Celestials are creatures native to the Upper Planes, and good by nature.'), 
-        ('Construct', 'A construct is either an animated object of some sort, or an artificially crafted creature. They are usually unintelligent, and immune to mind-influencing effects. They are immune to many effects that harm living creatures, such as poison and disease, but they cannot be magically restored to life. Constructs usually have darkvision.'),
-        ('Dragon', 'A dragon is a reptilian creature, usually winged, often having supernatural abilities. Dragons cannot be paralyzed or induced to sleep. Dragons usually have darkvision and low-light vision.'),
-        ('Elemental', 'An elemental is composed of one of the four classical elements of air, earth, fire, or water. They can resist effects such as poison and paralysis, and do not have any discernable front or back, making tactics such as flanking or attacks such as critical hits useless. Like constructs, they cannot be resurrected by magic once destroyed, save powerful spells such as wish or miracle. Elementals usually have darkvision.'),
-        ('Fey', 'Fey are creatures of magic with a connection to nature. Often they are connected to the Feywild, or certain Outer Planes such as Arborea or the Beastlands.'),
-        ('Fiend', 'Fiends are creatures native to the Lower Planes, and evil by nature.'),
-        ('Giant', 'A giant is a humanoid-shaped creature of great strength and size. Giants usually have darkvision.'),
-        ('Humanoid', 'Humanoids are the main people of most worlds, bipeds with culture but few natural magical abilities.'),
-        ('Monstrosity', 'Monstrosities are unnatural creatures from a variety of origins, including curses and magical experimentation.'),
-        ('Ooze',	'An ooze is an amorphous or mutable creature. Oozes are usually mindless, and thus immune to mind-influencing effects. They possess blindsight but are otherwise blind. Like elementals, they have no clear front or back and are thus immune to flanking or critical hits. Composed mainly of protoplasm, oozes resist effects such as poison and polymorphing.'),
-        ('Plant', 'A plant is a vegetable and fungus creature, and immune to effects such as poison, stunning, or polymorphing. They also ignore mind-influencing effects, and may have low-light vision (assuming they can see at all).'),
-        ('Undead', 'An undead is a once-living creature animated by spiritual or supernatural forces. They resist many effects which harm the living, such as poison and disease. They are healed by negative energy. Undead usually have darkvision.'),
-        ('Vermin', 'A vermin is an invertebrate, such as an insect, arachnid, arthropod, or worm. Vermin are unintelligent and immune to mind-influencing effects. They usually have darkvision.'),
-        ('Monstrous Humanoid', 'A monstrous humanoid is similar to a humanoid, but usually has monstrous or animalistic features. Many also have supernatural abilities. Monstrous humanoids usually have darkvision.'),
-        ('Goblinoid', 'Goblinoids had a typical humanoid anatomy, though their skin tone and texture was often somewhere between that of a human and a snail. The notable exception of this rule was the bugbears whose entire bodies were covered head to toe in thick fur.')`, 
-    (err, type) => {
-        if(err){
-            console.log("Insert failed");
-        }else{
-            console.log(type);
-        }
-});
-
 
 //close connection
 pool.end();
