b0f7356c957ea455e39a00057d4967a63d3bb706
nplawton <nplawton02@gmail.com>
2023-03-08 10:59:22 -0800

    second table added15

--- migrate.js
+++ migrate.js
@@ -12,39 +12,43 @@ pool.query(`DROP TABLE IF EXISTS type, creature`, (err, res) => {
 
 
 //foreign keys go last
-pool.query(`CREATE TABLE IF NOT EXISTS type (
-    type_id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    description text NOT NULL)`, 
-    (err, type) => {
-        if (err){
-            console.log("CREATE TABLE type failed");
-            console.log(err);
-        }else{
-            console.log("Type table created sucessfully");
-        }
-});
+setTimeout(() => {
+    pool.query(`CREATE TABLE IF NOT EXISTS type (
+        type_id SERIAL PRIMARY KEY NOT NULL,
+        name VARCHAR(200) NOT NULL,
+        description text NOT NULL)`, 
+        (err, type) => {
+            if (err){
+                console.log("CREATE TABLE type failed");
+                console.log(err);
+            }else{
+                console.log("Type table created sucessfully");
+            }
+    });
+}, 2000);
 
-pool.query(`CREATE TABLE IF NOT EXISTS creature (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    alignment_id INTEGER NOT NULL,
-    type_id INTEGER NOT NULL REFERENCES type (type_id),
-    health INTEGER NOT NULL,
-    exp INTEGER NOT NULL,
-    chal INTEGER,
-    descrip_id INTEGER NOT NULL,
-    stat_id INTEGER NOT NULL,
-    attack_id INTEGER NOT NULL,
-    spattack_id INTEGER NOT NULL)`, 
-    (err, creature) => {
-        if (err){
-            console.log("CREATE TABLE creature failed");
-            console.log(err);
-        }else{
-            console.log("Creatures table created sucessfully");
-        }
-});
+setTimeout(() => {
+    pool.query(`CREATE TABLE IF NOT EXISTS creature (
+        id SERIAL PRIMARY KEY NOT NULL,
+        name VARCHAR(200) NOT NULL,
+        alignment_id INTEGER NOT NULL,
+        type_id INTEGER NOT NULL REFERENCES type (type_id),
+        health INTEGER NOT NULL,
+        exp INTEGER NOT NULL,
+        chal INTEGER,
+        descrip_id INTEGER NOT NULL,
+        stat_id INTEGER NOT NULL,
+        attack_id INTEGER NOT NULL,
+        spattack_id INTEGER NOT NULL)`, 
+        (err, creature) => {
+            if (err){
+                console.log("CREATE TABLE creature failed");
+                console.log(err);
+            }else{
+                console.log("Creatures table created sucessfully");
+            }
+    });
+}, 4000)
 
 //close connection
 pool.end();
