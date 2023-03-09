61a8b7e70c3e75c0aff4b901e2212259f04af27f
nplawton <nplawton02@gmail.com>
2023-03-08 18:22:58 -0800

    3 table CRUD started4

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/type/:id', (req, res, next) => {
         res.status(404).send(`There is no creature type with id ${id}`);
     }
 
-    pool.query(`SELECT id, t_name, t_description FROM type WHERE id = $1`, [id], (err, results) => {
+    pool.query(`SELECT type_id, t_name, t_description FROM type WHERE id = $1`, [id], (err, results) => {
         
         if(err){
             return next(err);
