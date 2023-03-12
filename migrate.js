5317a7a617b9b867f386e76720cf8ac896387bc9
nplawton <nplawton02@gmail.com>
2023-03-12 11:51:14 -0700

    table creature test

--- migrate.js
+++ migrate.js
@@ -22,25 +22,22 @@ pool.query(`DROP TABLE IF EXISTS type, descrip, creature`)
     })
 .then((data) => {
         console.log('Creature Descrip table created sucessfully');
-    })
+        return pool.query(`CREATE TABLE IF NOT EXISTS creature (
+            id SERIAL PRIMARY KEY NOT NULL,
+            mon_name VARCHAR(200) NOT NULL,
+            alignment_id INTEGER NOT NULL,
+            type_id INTEGER NOT NULL REFERENCES type (type_id),
+            health INTEGER NOT NULL,
+            exp INTEGER NOT NULL,
+            chal INTEGER,
+            descrip_id INTEGER NOT NULL REFERENCES descrip (descrip_id),
+            stat_id INTEGER NOT NULL,
+            attack_id INTEGER NOT NULL,
+            spattack_id INTEGER NOT NULL)`);
+})
+.then((data) => {
+    console.log('Creatures table created sucessfully');
+})
 .catch((error) => {
     console.log('Error dropping tables:', error);
-})  
-// .then(pool.query(`CREATE TABLE IF NOT EXISTS creature (
-//     id SERIAL PRIMARY KEY NOT NULL,
-//     mon_name VARCHAR(200) NOT NULL,
-//     alignment_id INTEGER NOT NULL,
-//     type_id INTEGER NOT NULL REFERENCES type (type_id),
-//     health INTEGER NOT NULL,
-//     exp INTEGER NOT NULL,
-//     chal INTEGER,
-//     descrip_id INTEGER NOT NULL REFERENCES descrip (descrip_id),
-//     stat_id INTEGER NOT NULL,
-//     attack_id INTEGER NOT NULL,
-//     spattack_id INTEGER NOT NULL)`))
-// .then((data) => {
-//     console.log('Creatures table created sucessfully');
-// })
-// .catch((err) => {
-//     console.log('CREATE TABLE creature failed', err);
-// })
+})
