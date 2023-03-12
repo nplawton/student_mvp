37fdfd3d3d0c4b043de54745592c9198036bbc34
nplawton <nplawton02@gmail.com>
2023-03-12 11:33:02 -0700

    table descrip test9

--- migrate.js
+++ migrate.js
@@ -5,33 +5,30 @@ const pool = require('./dbConn');
 pool.query(`DROP TABLE IF EXISTS type, descrip, creature`)
     .then((data) => {
         console.log('Tables dropped!');
+        return pool.query(`CREATE TABLE IF NOT EXISTS type (
+            type_id SERIAL PRIMARY KEY NOT NULL,
+            t_name VARCHAR(200) NOT NULL,
+            t_description text NOT NULL)`);
     })
     .catch((error) => {
         console.log('Error dropping tables:', error);
     })
-.then(pool.query(`CREATE TABLE IF NOT EXISTS type (
-    type_id SERIAL PRIMARY KEY NOT NULL,
-    t_name VARCHAR(200) NOT NULL,
-    t_description text NOT NULL)`))
     .then((data) => {
         console.log('Creature Type table created sucessfully');
+        return pool.query(`CREATE TABLE IF NOT EXISTS descrip (
+            descrip_id SERIAL PRIMARY KEY NOT NULL,
+            size text,
+            speed text,
+            d_descrip text,
+            info text,
+            mon_img text)`);
     })
     .catch((error) => {
         console.log('CREATE TABLE type failed', error);
     })
-.then(pool.query(`CREATE TABLE IF NOT EXISTS descrip (
-        descrip_id SERIAL PRIMARY KEY NOT NULL,
-        size text,
-        speed text,
-        d_descrip text,
-        info text,
-        mon_img text)`))
-    .then((data) => {
-        console.log('Creature Descrip table created sucessfully');
-    })
-    .catch((error) => {
-        console.log('CREATE TABLE descrip failed', error);
-    })
+    // .then((data) => {
+    //     console.log('Creature Descrip table created sucessfully');
+    // })
 // .then(pool.query(`CREATE TABLE IF NOT EXISTS creature (
 //     id SERIAL PRIMARY KEY NOT NULL,
 //     mon_name VARCHAR(200) NOT NULL,
